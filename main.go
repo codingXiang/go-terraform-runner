5b3b6462a6f43a222964c53c325cadd3b1c309a5
xiang <dryadf66449@gmail.com>
2020-11-05 13:50:40 +0800

    modify: 修改範例

--- main.go
+++ main.go
@@ -4,11 +4,12 @@ import (
 	"github.com/codingXiang/configer"
 	"github.com/codingXiang/go-logger"
 	"github.com/codingXiang/go-orm"
-	"log"
+	"github.com/codingXiang/go-terraform-runner/command"
 	"github.com/codingXiang/go-terraform-runner/model"
 	"github.com/codingXiang/go-terraform-runner/module"
 	"github.com/codingXiang/go-terraform-runner/runner"
 	"github.com/codingXiang/go-terraform-runner/storage"
+	"log"
 )
 
 func init() {
@@ -49,36 +50,37 @@ func main() {
 		} else {
 			log.Fatal("上傳失敗，錯誤為", err)
 		}
-		runner.New(s, id).ModifyExistProject()
-		//r.AddStep(command.NewInitCommand(nil)).
-		//	AddStep(command.NewPlanCommand(nil)).
-		//	AddStep(command.NewApplyCommand(nil))
-		//if err := r.Run(); err != nil {
-		//	logger.Log.Error(err)
-		//}
-		//
-		//state := module.NewTerraformState(r.ID)
-		//
-		//if err := s.UpdateRecordState("config", id, state); err == nil {
-		//	log.Println("更新狀態成功，", string(state.GetRaw()))
-		//} else {
-		//	logger.Log.Error("更新狀態失敗，錯誤為", err)
-		//}
-		//
-		//logger.Log.Info("output =", string(state.GetRaw()))
-		//r.StepWorkflow()
-		//r.AddStep(command.NewPlanCommand(&command.Meta{Commands: []string{"-destroy"}})).
-		//	AddStep(command.NewApplyCommand(nil))
-		//if err := r.Run(); err != nil {
-		//	logger.Log.Error(err)
-		//}
-		//state = module.NewTerraformState(r.ID)
-		//if err := s.UpdateRecordState("config", id, state); err == nil {
-		//	log.Println("更新狀態成功，", string(state.GetRaw()))
-		//} else {
-		//	logger.Log.Error("更新狀態失敗，錯誤為", err)
-		//}
-		//r.Clean()
+		r := runner.New(s, id)
+		r.ModifyExistProject()
+		r.AddStep(command.NewInitCommand(nil)).
+			AddStep(command.NewPlanCommand(nil)).
+			AddStep(command.NewApplyCommand(nil))
+		if err := r.Run(); err != nil {
+			logger.Log.Error(err)
+		}
+
+		state := module.NewTerraformState(r.ID)
+
+		if err := s.UpdateRecordState("config", id, state); err == nil {
+			log.Println("更新狀態成功，", string(state.GetRaw()))
+		} else {
+			logger.Log.Error("更新狀態失敗，錯誤為", err)
+		}
+
+		logger.Log.Info("output =", string(state.GetRaw()))
+		r.StepWorkflow()
+		r.AddStep(command.NewPlanCommand(&command.Meta{Commands: []string{"-destroy"}})).
+			AddStep(command.NewApplyCommand(nil))
+		if err := r.Run(); err != nil {
+			logger.Log.Error(err)
+		}
+		state = module.NewTerraformState(r.ID)
+		if err := s.UpdateRecordState("config", id, state); err == nil {
+			log.Println("更新狀態成功，", string(state.GetRaw()))
+		} else {
+			logger.Log.Error("更新狀態失敗，錯誤為", err)
+		}
+		r.Clean()
 	}
 }
 
@@ -86,20 +88,22 @@ func getConfig(s *storage.ConfigSource) *module.Config {
 	const IDENTITY = "gitlab"
 	in := module.NewConfig()
 
-	//透過 IDENTITY 從資料庫中取得 Provider
-	p, err := s.GetProvider(IDENTITY, "")
-	//建立 Provider
-	provider := module.NewProviderWithVar(p)
+	//透過 provider 從資料庫中取得相關的 module
+	moduleVars, err := s.GetModule(nil, "group")
 	if err != nil {
 		panic(err)
 	}
-	//透過 Provider 建立 upgrade block
-	upgrade := module.NewUpgradeWithProvider(p)
-
-	//透過 provider 從資料庫中取得相關的 module
-	moduleVars, err := s.GetModule(p)
 	modules := make([]*module.ModuleEntity, 0)
 	for _, v := range moduleVars {
+		//透過 Provider 名稱 從資料庫中取得 Provider instance
+		p, _ := s.GetProvider(v.Provider.Name, "")
+		//轉換成 provider entity
+		provider := module.NewProviderWithVar(p)
+		//透過 provider 取得 upgrade
+		upgrade := module.NewUpgradeWithProvider(p)
+		//加入 provider
+		in.AddProvider(provider)
+		in.AddUpgrade(upgrade)
 		m := module.NewModuleWithVar(v).AddProvider(provider)
 		modules = append(modules, m)
 		in.AddMultipleVariable(m.Vars...)
@@ -108,7 +112,5 @@ func getConfig(s *storage.ConfigSource) *module.Config {
 	for _, m := range modules {
 		in.AddModule(m)
 	}
-	in.AddProvider(provider)
-	in.AddUpgrade(upgrade)
 	return in
 }
